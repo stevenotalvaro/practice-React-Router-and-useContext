87b90eb892f3a3efed57266ab877fd314ffe9114
Steven Otalvaro <stevenotalvaroz@gmail.com>
2021-06-30 14:31:07 -0500

    Router and Routes implemented

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {LoginScreen} from '../components/login/LoginScreen'
+import {DashboardRoutes} from './DashboardRoutes'
+export function AppRouter() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/login" component={LoginScreen}></Route>
+          <Route path="/" component={DashboardRoutes}></Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
---
+++ src/routers/DashboardRoutes.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Switch, Route, Redirect} from 'react-router-dom'
+import {DcScreen} from '../components/dc/DcScreen'
+import {HeroScreen} from '../components/heroes/HeroScreen'
+import {MarvelScreen} from '../components/marvel/MarvelScreen'
+import {Navbar} from '../components/ui/NavBar'
+export const DashboardRoutes = () => {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <Switch>
+          <Route exact path="/marvel" component={MarvelScreen}></Route>
+          <Route exact path="/hero/:heroId" component={HeroScreen}></Route>
+          <Route exact path="/dc" component={DcScreen}></Route>
+          <Redirect to="/marvel" />
+        </Switch>
+      </div>
+    </>
+  )
+}
