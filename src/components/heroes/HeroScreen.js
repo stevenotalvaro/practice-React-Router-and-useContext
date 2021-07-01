c80fb2d4a60436bc4c332c89ce29b33e039449c6
Steven Otalvaro <stevenotalvaroz@gmail.com>
2021-07-01 12:10:25 -0500

    Cards with animation and information implemented

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,6 +1,20 @@
 import React from 'react'
+import {Redirect, useParams} from 'react-router-dom'
+import {gerHeroById} from '../selectors/getHeroById'
 
 export const HeroScreen = () => {
+  const {heroId} = useParams()
+
+  const hero = gerHeroById(heroId)
+
+  if (!hero) {
+    return <Redirect to="/Marvel" />
+  }
+
+  const {id, superhero, publisher, alter_ego, first_appearance, characters} =
+    hero
+
+  console.log(hero)
   return (
     <div>
       <h1>Hero</h1>
