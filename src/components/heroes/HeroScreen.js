2270ae5c4e158c30bd059062537592b6e56e0121
Steven Otalvaro <stevenotalvaroz@gmail.com>
2021-07-01 12:26:48 -0500

    Info of every one Heroes Impmented

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Redirect, useParams} from 'react-router-dom'
 import {gerHeroById} from '../selectors/getHeroById'
 
-export const HeroScreen = () => {
+export const HeroScreen = ({history}) => {
   const {heroId} = useParams()
 
   const hero = gerHeroById(heroId)
@@ -14,10 +14,44 @@ export const HeroScreen = () => {
   const {id, superhero, publisher, alter_ego, first_appearance, characters} =
     hero
 
-  console.log(hero)
+  const handleReturn = () => {
+    if (history.length > 2) {
+      history.goBack()
+    }
+
+    history.push('/')
+  }
   return (
-    <div>
-      <h1>Hero</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={`../assets/heroes/${id}.jpg`}
+          alt={superhero}
+          className="img-thumbnail"
+        />
+      </div>
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            {' '}
+            <b>Alter ego: </b> {alter_ego}{' '}
+          </li>
+          <li className="list-group-item">
+            {' '}
+            <b> Publisher: </b> {publisher}{' '}
+          </li>
+          <li className="list-group-item">
+            {' '}
+            <b>First Appearance: </b> {first_appearance}{' '}
+          </li>
+        </ul>
+        <h5>Characters</h5>
+        <p>{characters}</p>
+        <button className="btn btn-primary" onClick={handleReturn}>
+          Return
+        </button>
+      </div>
     </div>
   )
 }
