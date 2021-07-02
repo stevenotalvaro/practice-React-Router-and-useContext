e354c5323a5fbb458e4bd69ef9d967633684ae91
Steven Otalvaro <stevenotalvaroz@gmail.com>
2021-07-02 00:14:54 -0500

    Route private implemented, for to anybody that want inside to route and isnt authenticated, the return back a login page

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {PrivateRoute} from './PrivateRoute'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import {LoginScreen} from '../components/login/LoginScreen'
 import {DashboardRoutes} from './DashboardRoutes'
+import {AuthContext} from '../auth/AuthContext'
 export function AppRouter() {
+  const {user} = useContext(AuthContext)
+  console.log(user)
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path="/login" component={LoginScreen}></Route>
-          <Route path="/" component={DashboardRoutes}></Route>
+          <Route exact path="/login" component={LoginScreen} />
+          <PrivateRoute
+            path="/"
+            component={DashboardRoutes}
+            isAuthenticated={user.logged}
+          />
         </Switch>
       </div>
     </Router>
