b5bd2dad2cf75a66c1771c1fd88f07536708de5e
Steven Otalvaro <stevenotalvaroz@gmail.com>
2021-07-01 23:26:19 -0500

    login and logout with information extra, share information amoung components

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {AuthContext} from '../../auth/AuthContext'
+import {types} from '../../types/types'
 
 export const LoginScreen = ({history}) => {
+  const {dispatch} = useContext(AuthContext)
+
   const handleClick = () => {
+    dispatch({
+      type: types.login,
+      payload: {
+        name: 'Steven',
+      },
+    })
     history.replace('/marvel')
   }
   return (
